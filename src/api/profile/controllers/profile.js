33ce204e70f2d25060f139b2d231e8a0cb1722d0
Rida Rehman <ridarehmanse@gmail.com>
2026-02-25 22:37:33 +0500

    Refactor to use Strapi users-permissions plugin for user management, update all relations, clean up schemas, add Dockerfile and signup route, and remove obsolete files

--- src/api/profile/controllers/profile.js
+++ src/api/profile/controllers/profile.js
@@ -8,4 +8,27 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::profile.profile', ({ strapi }) => ({
   // Custom controller methods
+  async signup(ctx) {
+    const { email, password, ...profileData } = ctx.request.body;
+    if (!email || !password) {
+      return ctx.badRequest('Email and password are required');
+    }
+    // 1. Create user in users-permissions plugin
+    const user = await strapi.plugins['users-permissions'].services.user.add({
+      email,
+      password,
+      username: email,
+      confirmed: true,
+      blocked: false,
+    });
+    // 2. Create profile and link to user
+    const profile = await strapi.entityService.create('api::profile.profile', {
+      data: {
+        ...profileData,
+        user: user.id,
+      },
+    });
+    // 3. Return created user and profile
+    ctx.send({ user, profile });
+  },
 }));
